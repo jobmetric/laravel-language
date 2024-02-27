b415e806cc37a2710adbe2afab82e2216197cb51
majid mohammadian <majeedmohammadian@gmail.com>
2024-02-27 19:17:26 +0330

    added view composer for share language variables

--- src/LanguageServiceProvider.php
+++ src/LanguageServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace JobMetric\Language;
 
+use Illuminate\Support\Facades\View;
+use JobMetric\Language\Facades\Language as LanguageFacade;
 use JobMetric\PackageCore\Exceptions\MigrationFolderNotFoundException;
 use JobMetric\PackageCore\Exceptions\RegisterClassTypeNotFoundException;
 use JobMetric\PackageCore\PackageCore;
@@ -23,5 +25,21 @@ class LanguageServiceProvider extends PackageCoreServiceProvider
             ->hasMigration()
             ->hasTranslation()
             ->registerClass('Language', Language::class);
+    }
+
+    /**
+     * after boot
+     *
+     * @return void
+     */
+    public function afterBootPackage(): void
+    {
+        $languages = LanguageFacade::all(['status' => true]);
+        View::composer('*', function ($view) use ($languages) {
+            $view->with('languages', $languages);
+
+            $defaultLanguage = $languages->where('locale', $this->app->getLocale())->first();
+            $view->with('languageInfo', $defaultLanguage);
+        });
     }
 }
