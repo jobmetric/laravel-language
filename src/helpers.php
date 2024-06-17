81c29d5caf7fc184eccc8eddbb54170d5684ef82
majid mohammadian <majeedmohammadian@gmail.com>
2024-06-17 20:11:26 +0330

    add helper functions

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,46 @@
 <?php
+
+use JobMetric\Language\Facades\Language;
+
+if (!function_exists('storeLanguage')) {
+    /**
+     * store language
+     *
+     * @param array $data
+     *
+     * @return array
+     */
+    function storeLanguage(array $data): array
+    {
+        return Language::store($data);
+    }
+}
+
+if (!function_exists('updateLanguage')) {
+    /**
+     * update language
+     *
+     * @param int $language_id
+     * @param array $data
+     *
+     * @return array
+     */
+    function updateLanguage(int $language_id, array $data): array
+    {
+        return Language::update($language_id, $data);
+    }
+}
+
+if (!function_exists('deleteLanguage')) {
+    /**
+     * delete language
+     *
+     * @param int $language_id
+     *
+     * @return array
+     */
+    function deleteLanguage(int $language_id): array
+    {
+        return Language::delete($language_id);
+    }
+}
