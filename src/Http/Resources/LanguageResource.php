d2f8cbf6e5934b22520c61dc8791ac87d8f01fbd
majid mohammadian <majeedmohammadian@gmail.com>
2024-06-17 20:10:56 +0330

    add language resource

---
+++ src/Http/Resources/LanguageResource.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace JobMetric\Language\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @property mixed id
+ * @property mixed name
+ * @property mixed flag
+ * @property mixed locale
+ * @property mixed direction
+ * @property mixed status
+ * @property mixed created_at
+ * @property mixed updated_at
+ */
+class LanguageResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'flag' => $this->flag,
+            'locale' => $this->locale,
+            'direction' => $this->direction,
+            'status' => $this->status,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at
+        ];
+    }
+}
