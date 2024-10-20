0df8f64f9cd3cf4b951812fe58aca51ff1af3d41
majidmohammadian <majeedmohammadian@gmail.com>
2024-10-20 21:13:34 +0330

    changed set language middleware

--- src/Http/Middleware/SetLanguageMiddleware.php
+++ src/Http/Middleware/SetLanguageMiddleware.php
@@ -11,18 +11,25 @@ use Symfony\Component\HttpFoundation\Response;
 class SetLanguageMiddleware
 {
     /**
-     * Handle an incoming request.
+     * Handle an incoming request for setting language.
      *
      * @param Request $request
      * @param Closure(Request): (Response) $next
+     * 
      * @return Response
      * @throws ContainerExceptionInterface
      * @throws NotFoundExceptionInterface
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if(request()->route()->getName() !== 'set-language') {
-            session()->has('language') ? app()->setLocale(session()->get('language')) : app()->setLocale(config('app.locale'));
+        if (request()->route()->getName() !== 'language.set') {
+            if (request()->header('Accept-Language')) {
+                app()->setLocale(request()->header('Accept-Language'));
+            } else if (session()->has('language')) {
+                app()->setLocale(session()->get('language'));
+            } else {
+                app()->setLocale(config('app.locale'));
+            }
         }
 
         return $next($request);
