74fed5a0a93a4c4cbc2c541ca2736df270bf53dd
majidmohammadian <majeedmohammadian@gmail.com>
2025-08-09 23:23:59 +0330

    added some calendar type

--- src/Enums/CalendarTypeEnum.php
+++ src/Enums/CalendarTypeEnum.php
@@ -7,6 +7,12 @@ use JobMetric\PackageCore\Enums\EnumMacros;
 /**
  * @method static GREGORIAN()
  * @method static JALALI()
+ * @method static HIJRI()
+ * @method static HEBREW()
+ * @method static BUDDHIST()
+ * @method static COPTIC()
+ * @method static ETHIOPIAN()
+ * @method static CHINESE()
  */
 enum CalendarTypeEnum: string
 {
@@ -14,4 +20,10 @@ enum CalendarTypeEnum: string
 
     case GREGORIAN = "gregorian";
     case JALALI = "jalali";
+    case HIJRI = "hijri";
+    case HEBREW = "hebrew";
+    case BUDDHIST = "buddhist";
+    case COPTIC = "coptic";
+    case ETHIOPIAN = "ethiopian";
+    case CHINESE = "chinese";
 }
