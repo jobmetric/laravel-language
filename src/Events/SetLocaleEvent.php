545f2d81d7124e0bc79c47ebc6679868d750d88e
majidmohammadian <majeedmohammadian@gmail.com>
2025-12-13 22:41:02 +0330

    added new dependency to event-system package from jobmetric and changed all events based on EventDomain in this package

--- src/Events/SetLocaleEvent.php
+++ src/Events/SetLocaleEvent.php
@@ -2,6 +2,39 @@
 
 namespace JobMetric\Language\Events;
 
-class SetLocaleEvent
+use JobMetric\EventSystem\Contracts\DomainEvent;
+use JobMetric\EventSystem\Support\DomainEventDefinition;
+
+readonly class SetLocaleEvent implements DomainEvent
 {
+    /**
+     * Create a new event instance.
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Returns the stable technical key for the domain event.
+     *
+     * @return string
+     */
+    public static function key(): string
+    {
+        return 'language.locale_set';
+    }
+
+    /**
+     * Returns the full metadata definition for this domain event.
+     *
+     * @return DomainEventDefinition
+     */
+    public static function definition(): DomainEventDefinition
+    {
+        return new DomainEventDefinition(self::key(), 'language::base.entity_names.language', 'language::base.events.locale_set.title', 'language::base.events.locale_set.description', 'fas fa-globe', [
+            'language',
+            'locale',
+            'localization',
+        ]);
+    }
 }
